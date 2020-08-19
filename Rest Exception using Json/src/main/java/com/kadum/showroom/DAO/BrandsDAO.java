52c89d9ea37953d5a687e25d0656dc5f079eea85
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-08-19 14:35:20 +0530

    REST exception handling using Json as response as added

---
+++ Rest Exception using Json/src/main/java/com/kadum/showroom/DAO/BrandsDAO.java
@@ -0,0 +1,56 @@
+package com.kadum.showroom.DAO;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kadum.showroom.model.Brand;
+
+public class BrandsDAO {
+
+	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Brand.class)
+			.buildSessionFactory();
+
+	public List<Brand> getBrands() {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		List<Brand> list = session.createQuery("from brands").getResultList();
+		return list;
+	}
+	
+	public Brand getBrand(int brandId) {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		return session.get(Brand.class, brandId);
+	}
+
+	public void addBrand(Brand brand) {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		session.save(brand);
+		session.getTransaction().commit();
+
+	}
+
+	public void updateBrand(Brand updatedBrand) {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		int id = updatedBrand.getBrandId();
+		Brand brand = session.get(Brand.class, id);
+		brand.setBrandName(updatedBrand.getBrandName());;
+		session.getTransaction().commit();
+
+	}
+
+	public void deleteBrand(int brandId) {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		Brand brand = session.get(Brand.class, brandId);
+		session.delete(brand);
+		session.getTransaction().commit();
+
+	}
+
+}
