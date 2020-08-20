89db621da7f8dfc2829889d967c483313897f024
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-08-20 21:01:13 +0530

    Project based on handling client GET request added

---
+++ Client Response Read/src/main/java/com/kadum/model/Brand.java
@@ -0,0 +1,46 @@
+package com.kadum.model;
+
+public class Brand {
+	private int brandId;
+	private String brandName;
+    
+	public Brand() {}
+	
+	public Brand(String brandName) {
+		super();
+		this.brandName = brandName;
+	}
+	
+
+	public Brand(int brandId, String brandName) {
+		super();
+		this.brandId = brandId;
+		this.brandName = brandName;
+	}
+ 
+	public int getBrandId() {
+		return brandId;
+	}
+
+
+	public void setBrandId(int brandId) {
+		this.brandId = brandId;
+	}
+
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Brand [brandId=" + brandId + ", brandName=" + brandName + "]";
+	}
+	
+
+}
