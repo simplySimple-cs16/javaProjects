4220ed4ee728f9c15c7d30f1133cee0056b0de04
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-08-20 18:52:15 +0530

    Projects related to client and url building added

---
+++ Building Client String Response/src/main/java/com/kadum/App.java
@@ -0,0 +1,28 @@
+package com.kadum;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("app")
+public class App {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {    
+
+        Client client = ClientBuilder.newClient();
+        String response =  client.target("http://localhost:8080/restHateoas3/showroom/brands")
+       		 .request(MediaType.APPLICATION_JSON)
+       		 .get(String.class); 
+        return response;
+		
+	}
+
+}
---
+++ Client URL Building/src/main/java/com/kadum/App.java
@@ -0,0 +1,36 @@
+package com.kadum;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("app")
+public class App {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {    
+
+        
+        Client client = ClientBuilder.newClient();
+        WebTarget baseBrandURL =  client.target("http://localhost:8080/restHateoas3/showroom/brands");
+        WebTarget brandURL = baseBrandURL.path("{brandId}");
+        
+             
+        String response = brandURL
+       		           .resolveTemplate("brandId", "3")
+       		           .request(MediaType.APPLICATION_JSON)
+       		           .get(String.class);
+      
+        return response;
+        
+	}
+
+}
