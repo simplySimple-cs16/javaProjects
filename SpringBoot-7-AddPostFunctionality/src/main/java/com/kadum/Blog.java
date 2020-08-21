12322682d00764c28fd419a3b81f1797e0806104
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-08-21 17:25:55 +0530

    Adding post functionality added to blog project

---
+++ SpringBoot-7-AddPostFunctionality/src/main/java/com/kadum/Blog.java
@@ -0,0 +1,13 @@
+package com.kadum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog.class, args);
+	}
+
+}
---
+++ SpringBoot-7-AddPostFunctionality/src/main/java/com/kadum/controller/PostsController.java
@@ -0,0 +1,32 @@
+package com.kadum.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kadum.entity.Post;
+import com.kadum.services.PostsService;
+
+@RestController
+public class PostsController {
+	
+	@RequestMapping("/posts")
+    public List<Post> getPosts(){
+    	return new PostsService().getPosts();
+    }
+	
+	@RequestMapping("/posts/{id}")
+	public Post getPost(@PathVariable int id) {
+		return new PostsService().getPost(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/posts")
+	public void addPost(@RequestBody Post listElement) {
+		new PostsService().addPost(listElement);
+	}
+	
+}
