ef0c27e481ea419e23258fce86480269dd9963bc
simplySimple-cs16 <tensedstudent123654@gmail.com>
2020-08-21 11:33:35 +0530

    Project based on SpringBoot webapp and jsp support added

---
+++ SpringBoot-WebApp/src/main/java/com/kadum/WebController.java
@@ -0,0 +1,13 @@
+package com.kadum;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	@RequestMapping("welcome")  //=>path -> baseUrl/welcome
+	public String welcome() {
+	 return "welcome.html";
+	}
+
+}
